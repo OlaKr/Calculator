3af9fbd22a84b9f4803137e670b15260b65d6520
jestemlidzia <lidkaa.wu@gmail.com>
2021-01-19 00:27:38 +0100

    led

--- main.c
+++ main.c
@@ -32,7 +32,23 @@ void PIT_IRQHandler(){
 }
 
 int main(void) {
+	
+	//dioda
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	PORTB->PCR[13] |= PORT_PCR_MUX(1); /* MUX cónfig. Set Pin 8 óf PORT B as GPIO */
+	PTB->PDDR |= (1<<13);
+	
+	//PTB->PDOR&=~(1<<13); //off
+	PTB->PDOR|=(1<<13); //on
 
+	//buzzer
+	PORTB->PCR[5] |= PORT_PCR_MUX(1); /* MUX cónfig. Set Pin 8 óf PORT B as GPIO */
+	PTB->PDDR |= (1<<5 );
+	PORTB->PCR[5] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	//PTB->PDOR&=~(1<<13); //off
+	PTB->PDOR|=(1<<5); //on
+
+	
 	LCD1602_Init(); /* initialize LCD */
 	LCD1602_Backlight(TRUE);
 	rtc_init();
@@ -45,14 +61,15 @@ int main(void) {
 			{
 				for (int i = 1; i < 5; i++)
 				{
-			//			choose_row(i);
-			//		if(print_button(i) != 0)
-			//			calculator(print_button(i));
+						choose_row(i);
+					if(print_button(i) != 0)
+						calculator(print_button(i));
 					
 				}
 				LCD1602_SetCursor(0,0);
 				sprintf(temp, "%i", sec);
 	LCD1602_Print(temp);
+			//	rtc_reset();
 				irqTimer=0;
 			}
 			
